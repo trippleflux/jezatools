10138cbe7e8aa7ca065d271b7d764735a17b823f
andrej.jeznik <andrej.jeznik@fdea0f5c-7643-0410-ac65-19a450a61724>
2009-07-17 06:42:08 +0000

    Reformat code

--- trunk/ioFTPD/ioFTPD.ZipScript/Program.cs
+++ trunk/ioFTPD/ioFTPD.ZipScript/Program.cs
@@ -1,12 +1,12 @@
 ﻿namespace ioFTPD.ZipScript
 {
-    class Program
+    public class Program
     {
-        static int Main(string[] args)
+        public static int Main (string[] args)
         {
-            ConsoleApp consoleApp = new ConsoleApp(args);
-            consoleApp.Parse();
-            return consoleApp.Process() ? 0 : 1;
+            ConsoleApp consoleApp = new ConsoleApp (args);
+            consoleApp.Parse ();
+            return consoleApp.Process () ? 0 : 1;
         }
     }
 }
