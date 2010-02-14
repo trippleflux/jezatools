63e9bbb09caff2f6b003e5f0c6bb94469701821d
andrej.jeznik <andrej.jeznik@fdea0f5c-7643-0410-ac65-19a450a61724>
2010-02-14 10:00:27 +0000

    [ADD] Versioning from nant

--- trunk/ioFTPD/jeza.ioFTPD.Tests/Properties/AssemblyInfo.cs
+++ trunk/ioFTPD/jeza.ioFTPD.Tests/Properties/AssemblyInfo.cs
@@ -1,25 +1,29 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+#region
+using System.Reflection;
 using System.Runtime.InteropServices;
 
+#endregion
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+
 [assembly: AssemblyTitle("ioFTPD.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("JezaSoft")]
 [assembly: AssemblyProduct("ioFTPD.Tests")]
-[assembly: AssemblyCopyright("Copyright © JezaSoft 2009")]
+[assembly: AssemblyCopyright("Copyright JezaSoft 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
+
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
+
 [assembly: Guid("2b0ce191-d108-4f22-a265-352d2d0fc1bd")]
 
 // Version information for an assembly consists of the following four values:
@@ -32,5 +36,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
+
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
