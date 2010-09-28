505061e733e1b4d8b0bf6c77d4eec903fa530def
andrej.jeznik <andrej.jeznik@gmail.com>
2010-09-28 12:40:20 +0000

    [ADD] Start with mp3 sorting

---
+++ ioFTPD/jeza.ioFTPD.Tests/Media/Mp3Sort.cs
@@ -0,0 +1,26 @@
+using MbUnit.Framework;
+
+namespace jeza.ioFTPD.Tests.Media
+{
+    [TestFixture]
+    public class Mp3Sort
+    {
+        [Test]
+        public void ByArtist()
+        {
+            
+        }
+
+        [Test]
+        public void ByGenre()
+        {
+
+        }
+
+        [Test]
+        public void ByYear()
+        {
+
+        }
+    }
+}
