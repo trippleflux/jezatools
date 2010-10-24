c5ee73f0ec29c2cf7b360bdde2e5f6ceae8fe4df
andrej.jeznik <andrej.jeznik@fdea0f5c-7643-0410-ac65-19a450a61724>
2010-10-24 09:49:01 +0000

    [ADD] User management

--- trunk/ProductTracker/ProductTracker.Web/Login.aspx.cs
+++ trunk/ProductTracker/ProductTracker.Web/Login.aspx.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿#region
+
+using System;
 using System.Web.Security;
 using System.Web.UI.WebControls;
+
+#endregion
 
 namespace ProductTracker.Web
 {
@@ -8,19 +12,38 @@ namespace ProductTracker.Web
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
         }
 
         protected void Login_Authenticate(object sender, AuthenticateEventArgs e)
         {
-            if (FormsAuthentication.Authenticate(LoginControl.UserName, LoginControl.Password))
+            string typedPassword = Misc.ConvertToSha1(LoginControl.Password.Trim());
+            if (CheckPassword(LoginControl.UserName, typedPassword))
             {
                 FormsAuthentication.RedirectFromLoginPage(LoginControl.UserName, LoginControl.RememberMeSet);
             }
+                //if (FormsAuthentication.Authenticate(LoginControl.UserName, LoginControl.Password))
+                //{
+                //    FormsAuthentication.RedirectFromLoginPage(LoginControl.UserName, LoginControl.RememberMeSet);
+                //}
             else
             {
                 Response.Write("Invalid credentials");
             }
+        }
+
+        private static bool CheckPassword(string username, string password)
+        {
+            DataBase dataBase = new DataBase();
+            User user = dataBase.GetUser(username);
+            if (user == null)
+            {
+                return false;
+            }
+            if (user.Name.Equals(username) && user.Password.Equals(password))
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
---
+++ trunk/ProductTracker/ProductTracker.Web/ManageUsers.aspx.cs
@@ -0,0 +1,83 @@
+﻿#region
+
+using System;
+using System.Collections.Generic;
+
+#endregion
+
+namespace ProductTracker.Web
+{
+    public partial class ManageUsers : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SetElementValues();
+            LabelStatus.Text = "";
+            if (!IsPostBack)
+            {
+                PopulateUsers();
+            }
+        }
+
+        private void PopulateUsers()
+        {
+            DataBase dataBase = new DataBase();
+            IList<User> users = dataBase.GetUsers();
+            GridViewUsers.DataSource = users;
+            GridViewUsers.DataBind();
+        }
+
+        private void SetElementValues()
+        {
+            SettingsManager settingsManager = new SettingsManager();
+            Settings settings = settingsManager.DeserializeFromXml();
+            foreach (Page page in settings.Page)
+            {
+                if (page.Id == "ManageUsers")
+                {
+                    List<Setting> setting = page.Setting;
+                    HyperLinkMain.Text = settingsManager.GetSettingValue("HyperLinkMain", setting);
+                    LabelUserName.Text = settingsManager.GetSettingValue("LabelUsername", setting);
+                    LabelPassword.Text = settingsManager.GetSettingValue("LabelPassword", setting);
+                    LabelLevel.Text = settingsManager.GetSettingValue("LabelLevel", setting);
+                    LinkButtonAddUser.Text = settingsManager.GetSettingValue("LabelLevel", setting);
+                    statusWrongUsername = settingsManager.GetSettingValue("StatusWrongUsername", setting);
+                    statusWrongPassword = settingsManager.GetSettingValue("StatusWrongPassword", setting);
+                    statusFailedToAddUser = settingsManager.GetSettingValue("StatusFailedToAddUser", setting);
+                }
+            }
+        }
+
+        protected void LinkButtonAddUser_Click(object sender, EventArgs e)
+        {
+            LabelStatus.Text = "";
+            string username = TextBoxUserName.Text.Trim();
+            if (username.Length < 3)
+            {
+                LabelStatus.Text = statusWrongUsername;
+                return;
+            }
+            string password = TextBoxPassword.Text.Trim();
+            if (password.Length < 3)
+            {
+                LabelStatus.Text = statusWrongPassword;
+                return;
+            }
+            User user = new User {Name = username, Password = Misc.ConvertToSha1(password), Level = 2};
+            DataBase dataBase = new DataBase();
+            if (dataBase.InsertUser(user))
+            {
+                PopulateUsers();
+                LabelStatus.Text = "";
+            }
+            else
+            {
+                LabelStatus.Text = statusFailedToAddUser;
+            }
+        }
+
+        private string statusWrongUsername = "Username must be at least 3 chars long and can not contain spaces!";
+        private string statusWrongPassword = "Password must be at least 3 chars long and can not contain spaces!";
+        private string statusFailedToAddUser = "Failed to add user!";
+    }
+}
