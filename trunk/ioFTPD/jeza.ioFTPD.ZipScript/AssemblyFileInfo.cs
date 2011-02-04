93080c49d5c18f67b1dd24f356ae5d9f37b87440
andrej.jeznik <andrej.jeznik@fdea0f5c-7643-0410-ac65-19a450a61724>
2011-02-04 21:13:16 +0000

    [CHG] Versioning

---
+++ trunk/ioFTPD/jeza.ioFTPD.ZipScript/AssemblyFileInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Gallio.Framework;
+using MbUnit.Framework;
+using MbUnit.Framework.ContractVerifiers;
+
+namespace jeza.ioFTPD.ZipScript
+{
+    [TestFixture]
+    public class AssemblyFileInfo
+    {
+        [Test]
+        public void Test()
+        {
+            //
+            // TODO: Add test logic here
+            //
+        }
+    }
+}
