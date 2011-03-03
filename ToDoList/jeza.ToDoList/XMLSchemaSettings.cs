d7dac960a01b10d0d9f9ed7439be659771e8d872
andrej.jeznik <andrej.jeznik@gmail.com>
2011-03-03 12:45:40 +0000

    [ADD] New project for task management

---
+++ ToDoList/jeza.ToDoList/XMLSchemaSettings.cs
@@ -0,0 +1,27 @@
+﻿namespace jeza.ToDoList
+{
+    [System.SerializableAttribute]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true, Namespace = XmlSchemaNamespace)]
+    [System.Xml.Serialization.XmlRootAttribute(ElementName = "settings", Namespace = XmlSchemaNamespace, IsNullable = false)]
+    public class Settings
+    {
+        private const string XmlSchemaNamespace = "http://tempuri.org/XMLSchemaSettings.xsd";
+
+        [System.Xml.Serialization.XmlElementAttribute("googleAccount")]
+        public GoogleAccountInfo[] GoogleAccount { get; set; }
+    }
+
+    [System.SerializableAttribute]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true, Namespace = "http://tempuri.org/XMLSchemaSettings.xsd")]
+    public class GoogleAccountInfo
+    {
+        [System.Xml.Serialization.XmlElementAttribute("title")]
+        public string Title { get; set; }
+
+        [System.Xml.Serialization.XmlElementAttribute("username")]
+        public string Username { get; set; }
+
+        [System.Xml.Serialization.XmlElementAttribute("password")]
+        public string Password { get; set; }
+    }
+}
