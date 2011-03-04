484a002cb5b097b9b742b27e382c4e20da427c63
andrej.jeznik <andrej.jeznik@gmail.com>
2011-03-04 13:41:54 +0000

    [ADD] Gui

---
+++ ToDoList/jeza.ToDoList.Gui/Home.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Windows.Controls;
+
+namespace jeza.ToDoList.Gui
+{
+    /// <summary>
+    /// Interaction logic for Home.xaml
+    /// </summary>
+    public partial class Home : Page
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
