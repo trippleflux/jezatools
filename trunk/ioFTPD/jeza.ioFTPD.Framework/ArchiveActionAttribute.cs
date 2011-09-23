6b46000857e492a67579e47cce8c78fb93c92f07
andrej.jeznik@gmail.com <andrej.jeznik@gmail.com@fdea0f5c-7643-0410-ac65-19a450a61724>
2011-09-23 08:05:11 +0000

    [ADD] New action for archiving

--- trunk/ioFTPD/jeza.ioFTPD.Framework/ArchiveActionAttribute.cs
+++ trunk/ioFTPD/jeza.ioFTPD.Framework/ArchiveActionAttribute.cs
@@ -2,9 +2,25 @@ namespace jeza.ioFTPD.Framework
 {
     public enum ArchiveActionAttribute
     {
+        /// <summary>
+        /// Total space used on DISK.
+        /// </summary>
         TotalUsedSpace,
+        /// <summary>
+        /// Total used space in specified folder.
+        /// </summary>
+        TotalFolderUsedSpace,
+        /// <summary>
+        /// Total free space on DISK is belov value specified.
+        /// </summary>
         TotalFreeSpace,
+        /// <summary>
+        /// Total folder count in specified fodler.
+        /// </summary>
         TotalFolderCount,
+        /// <summary>
+        /// If folder is older than the number of days specified.
+        /// </summary>
         DateOlder,
     }
 }
