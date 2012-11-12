d626770d1934ec378d34f9eac16fb62656e9db19
andrej.jeznik <andrej.jeznik@gmail.com>
2012-11-12 20:24:26 +0000

    [FIX] Corrected logging and write to .ioFTPD.Message file

--- ioFTPD/AssemblyFileInfo.cs
+++ ioFTPD/AssemblyFileInfo.cs
@@ -21,7 +21,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(false)]
 [assembly: Guid("d038566a-1937-478a-b5c5-b79c4afb253d")]
-[assembly: AssemblyVersion("4.6.50.621")]
-[assembly: AssemblyFileVersion("4.6.50.621")]
+[assembly: AssemblyVersion("4.6.50.622")]
+[assembly: AssemblyFileVersion("4.6.50.622")]
 
 
