08251a4124f90ac287a170c546a6e89cfd0a6992
andrej.jeznik <andrej.jeznik@gmail.com>
2013-12-27 19:40:00 +0000

    [ADD] FindWindow method

---
+++ ioFTPD/jeza.ioFTPD.Framework/ioFTPD/SharedMemoryManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace jeza.ioFTPD.Framework.ioFTPD
+{
+    public class SharedMemoryManager
+    {
+        private readonly string m_ioFtpdMessageWindow;
+        private Process m_currentProcess;
+        private bool m_isWindowThere;
+
+        public SharedMemoryManager()
+        {
+            m_ioFtpdMessageWindow = "ioFTPD::MessageWindow";
+            Initialize();
+        }
+
+        public SharedMemoryManager(string ioftpdMessagewindow)
+        {
+            m_ioFtpdMessageWindow = ioftpdMessagewindow;
+            m_isWindowThere = false;
+            FindWindow();
+        }
+
+        public bool IsWindowThere
+        {
+            get { return m_isWindowThere; }
+        }
+
+        [DllImport("User32.Dll")]
+        private static extern IntPtr FindWindow(string lpClassName,
+                                                string lpWindowName);
+
+        private void Initialize()
+        {
+            m_isWindowThere = false;
+            m_currentProcess = Process.GetCurrentProcess();
+        }
+
+        // Find window by Caption, and wait 1/2 a second and then try again.
+        private void FindWindow(int waitTime = 500)
+        {
+            const int maxRetryCount = 2;
+            int retryCount = 0;
+            IntPtr hWnd = FindWindow(m_ioFtpdMessageWindow, null);
+            while (retryCount <= maxRetryCount && hWnd == IntPtr.Zero)
+            {
+                Thread.Sleep(waitTime);
+                hWnd = FindWindow(m_ioFtpdMessageWindow, null);
+                retryCount++;
+            }
+            m_isWindowThere = hWnd != IntPtr.Zero;
+        }
+    }
+}
