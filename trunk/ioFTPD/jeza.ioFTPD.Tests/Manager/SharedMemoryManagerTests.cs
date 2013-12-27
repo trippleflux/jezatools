83dfeeff8ffd11960bac0d4c2de04ee093f78278
andrej.jeznik@gmail.com <andrej.jeznik@gmail.com@fdea0f5c-7643-0410-ac65-19a450a61724>
2013-12-27 19:40:00 +0000

    [ADD] FindWindow method

---
+++ trunk/ioFTPD/jeza.ioFTPD.Tests/Manager/SharedMemoryManagerTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using jeza.ioFTPD.Framework.ioFTPD;
+
+namespace jeza.ioFTPD.Tests.Manager
+{
+    [TestFixture]
+    public class SharedMemoryManagerTests
+    {
+        [Test]
+        [Ignore]
+        public void FindWindow()
+        {
+            const string ioftpdMessagewindow = "ioFTPD::MessageWindow";
+            SharedMemoryManager sharedMemoryManager = new SharedMemoryManager(ioftpdMessagewindow);
+            bool isWindowThere = sharedMemoryManager.IsWindowThere;
+            Assert.IsTrue(isWindowThere, string.Format("'{0}' was not found!", ioftpdMessagewindow));
+        }
+    }
+}
